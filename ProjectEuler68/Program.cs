0b0f326037e881e3d5f1fb32398d817e9b15a2ac
Github User <this@isafakeadress.lose.tld>
2013-06-22 11:50:31 +0100

    Verification: works with 3gons
    used the indexing system shown in the problem description. gets the
    right answer in no time at all. for 5gons will have different indexing

--- ProjectEuler68/Program.cs
+++ ProjectEuler68/Program.cs
@@ -8,6 +8,36 @@ namespace ProjectEuler68
 {
     class Program
     {
+
+        static bool IsMagic3Gon(int[] x)
+        {
+            bool answer = true;
+
+            int sum = x[4 - 1] + x[3 - 1] + x[2 - 1];
+
+            if (sum != x[1 - 1] + x[2 - 1] + x[6 - 1]) answer = false;
+            if (sum != x[5 - 1] + x[1 - 1] + x[3 - 1]) answer = false;
+
+            return answer;
+        }
+
+        static long Value3Gon(int[] x)
+        {
+            long answer = 0;
+            answer += x[4-1]; answer *= 10;
+            answer += x[3-1]; answer *= 10;
+            answer += x[2-1]; answer *= 10;
+
+            answer += x[6-1]; answer *= 10;
+            answer += x[2-1]; answer *= 10;
+            answer += x[1-1]; answer *= 10;
+
+            answer += x[5-1]; answer *= 10;
+            answer += x[1-1]; answer *= 10;
+            answer += x[3-1];
+            return answer;
+        }
+
         static void Main(string[] args)
         {
 
@@ -27,23 +57,45 @@ namespace ProjectEuler68
 
             int k = 0, l = 0, m = 0, Temp;
 
-            long accum = 0, ToAdd = 0;
+            bool KeepGoing = true, NextIsLast = false;
 
-            while (!Perm.SequenceEqual(Final))
+            long answer = -1;
+
+            while (KeepGoing)
             {
-                k = -1;
-                for (m = 0; m < Initial.Length - 1; m++)
-                    if (Perm[m] < Perm[m + 1])
-                        k = m;
-                l = -1;
-                for (m = k; m < Initial.Length; m++)
-                    if (Perm[k] < Perm[m])
-                        l = m;
-                Temp = Perm[k];
-                Perm[k] = Perm[l];
-                Perm[l] = Temp;
-                Array.Reverse(Perm, k + 1, Initial.Length - (k + 1));
+
+                if (IsMagic3Gon(Perm) && Perm[3] < Perm[4] && Perm[3] < Perm[5])
+                {
+                    if (Value3Gon(Perm) > answer)
+                    {
+                        answer = Value3Gon(Perm);
+                    }
+                }
+
+                if (!NextIsLast)
+                {
+                    k = -1;
+                    for (m = 0; m < Initial.Length - 1; m++)
+                        if (Perm[m] < Perm[m + 1])
+                            k = m;
+                    l = -1;
+                    for (m = k; m < Initial.Length; m++)
+                        if (Perm[k] < Perm[m])
+                            l = m;
+                    Temp = Perm[k];
+                    Perm[k] = Perm[l];
+                    Perm[l] = Temp;
+                    Array.Reverse(Perm, k + 1, Initial.Length - (k + 1));
+
+                    if (Perm.SequenceEqual(Final))
+                        NextIsLast = true;
+                }
+                else
+                {
+                    KeepGoing = false;
+                }
             }
+            Console.WriteLine(answer);
         }
     }
 }
